0a93b182890c43c7a71cdb4d99125c829e140055
Britt Crawford <britt@illtemper.org>
2007-03-07 22:25:47 +0000

    removed empty test
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@190 8f33570f-f526-0410-8023-93ba2dbcbd24

--- src/test/java/org/hivedb/management/quartz/TestNodeBalancer.java
+++
@@ -1,9 +0,0 @@
-package org.hivedb.management.quartz;
-
-import junit.framework.TestCase;
-
-public class TestNodeBalancer extends TestCase {
-	public void testSuggestMoves() {
-		// TODO Method Stub
-	}
-}
