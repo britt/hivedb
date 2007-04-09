8c56a0a3d3c442a19544e0a48246bd66d2569e85
Britt Crawford <britt@illtemper.org>
2007-04-09 22:48:07 +0000

    The connection pool size for HiveBasicDataSource is now configurable via System property
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@256 8f33570f-f526-0410-8023-93ba2dbcbd24

--- src/main/java/org/hivedb/meta/persistence/HiveBasicDataSource.java
+++ src/main/java/org/hivedb/meta/persistence/HiveBasicDataSource.java
@@ -3,10 +3,26 @@ package org.hivedb.meta.persistence;
 import org.apache.commons.dbcp.BasicDataSource;
 
 public class HiveBasicDataSource extends BasicDataSource {
+	public static final String CONNECTION_POOL_SIZE = "HiveDataSourceConnectionPoolSize";
+	public static final int DEFAULT_POOL_SIZE = 32;
 
+	public HiveBasicDataSource(String hiveUri, int poolSize){
+		super();
+		setUrl(hiveUri);	
+		setMaxActive(poolSize);
+	}
+	
 	public HiveBasicDataSource(String hiveUri)
 	{
-		super();
-		setUrl(hiveUri);		
+		this(hiveUri, getDefaultPoolSize());	
+	}
+	
+	private static Integer getDefaultPoolSize() {
+		try {
+			String poolSize = System.getProperty(CONNECTION_POOL_SIZE);
+			return Integer.parseInt(poolSize);
+		} catch (Exception e) {
+			return DEFAULT_POOL_SIZE;
+		}
 	}
 }
