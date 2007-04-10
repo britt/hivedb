3bbe7dce2ea449d73fc119bb378c34ca19e07569
Justin McCarthy <jmccarthy@cafepress.com>
2007-04-10 17:36:20 +0000

    Checking in updates, however I believe this class is obselete
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@258 8f33570f-f526-0410-8023-93ba2dbcbd24

--- src/main/java/org/hivedb/management/HttpAdaptorManager.java
+++ src/main/java/org/hivedb/management/HttpAdaptorManager.java
@@ -1,39 +1,53 @@
 package org.hivedb.management;
 
+import java.io.IOException;
+import java.util.Collection;
+
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
+import mx4j.tools.adaptor.http.HttpAdaptor;
+import mx4j.tools.adaptor.http.XSLTProcessor;
+
+import org.apache.log4j.Logger;
 import org.springframework.jmx.export.MBeanExporterListener;
 
 public class HttpAdaptorManager implements MBeanExporterListener {
+	Logger log = Logger.getLogger(HttpAdaptorManager.class);
 	private MBeanServer mbeanServer;
-	private String adaptorSubstring = "httpadaptor";
+	private Collection<HttpAdaptor> adaptors = null;
 
 	public void mbeanRegistered(ObjectName objectName) {
-		if (objectName.getCanonicalName().toLowerCase().contains(adaptorSubstring)) {
-			try {
-				mbeanServer.invoke(objectName, "start", null, null);
-			} catch (Exception e) {
-				System.err.println("Can't start HttpAdaptor: " + e);
-			}
-		}
+		// TODO: I don't think we need to be an MBeanExporterListener any longer, as the Spring config 
+		// is explicitly invoking start() on the HttpAdaptors via this.startAdaptors()  -jm
 	}
 
 	public void mbeanUnregistered(ObjectName objectName) {
-		if (objectName.getCanonicalName().toLowerCase().contains(adaptorSubstring)) {
-			try {
-				mbeanServer.invoke(objectName, "stop", null, null);
-			} catch (Exception e) {
-				System.err.println("Can't stop HttpAdaptor: " + e);
-			}
+		// TODO: I don't think we need to be an MBeanExporterListener any longer, as the Spring config 
+		// is explicitly invoking start() on the HttpAdaptors via this.startAdaptors()  -jm
+	}
+	
+	public void startAdaptors() {
+		if (this.mbeanServer==null) {
+			log.error("The HttpAdaptorManager init method (startAdaptors) was called before a reference to the MBeanServer was set");
 		}
+		for (HttpAdaptor adaptor : adaptors)
+			try {
+				adaptor.setProcessor(new XSLTProcessor());
+				adaptor.preRegister(this.mbeanServer, new ObjectName("Foo:bar=beef"));
+				adaptor.start();
+			} catch (IOException e) {
+				log.error("Can't start HttpAdaptor: " + e);
+			} catch (Exception e) {
+				log.error("Can't start HttpAdaptor: " + e);
+			}
+	}
+	
+	public void setHttpAdaptors(Collection<HttpAdaptor> adaptors) {
+		this.adaptors = adaptors;
 	}
 
 	public void setMbeanServer(MBeanServer mbeanServer) {
 		this.mbeanServer = mbeanServer;
-	}
-
-	public void setAdaptorName(String adaptorName) {
-		this.adaptorSubstring = adaptorName;
 	}
 }
