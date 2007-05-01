d8a58cb033cc58277d7a4ebf0225f5325d9b6f5c
Britt Crawford <britt@illtemper.org>
2007-05-01 21:00:25 +0000

    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@298 8f33570f-f526-0410-8023-93ba2dbcbd24

--- src/main/java/org/hivedb/util/database/MysqlTestCase.java
+++ src/main/java/org/hivedb/util/database/MysqlTestCase.java
@@ -9,66 +9,73 @@ import org.hivedb.util.functional.Unary;
 import org.testng.annotations.BeforeMethod;
 
 public class MysqlTestCase {
-	
-	// Data nodes are inserted in the hive's node_metadata table. The databases are never actually created,
+	protected boolean recycleDatabase = true;
+
+	// Data nodes are inserted in the hive's node_metadata table. The databases
+	// are never actually created,
 	// since the hive never interacts directly with the data nodes.
-	protected String[] dataNodes = new String[] { "data1", "data2", "data3" }; 
-	
+	protected String[] dataNodes = new String[] { "data1", "data2", "data3" };
+
 	public Collection<String> getDataUris() {
 		return Transform.map(new Unary<String, String>() {
-		public String f(String dataNodeName) {
-			return getDataNodeConnectString(dataNodeName);
-		}},
-		Arrays.asList(dataNodes));
+			public String f(String dataNodeName) {
+				return getDataNodeConnectString(dataNodeName);
+			}
+		}, Arrays.asList(dataNodes));
 	}
 
 	@BeforeMethod
 	public void setUp() {
 		recycleDatabase();
 	}
-	
-	private String getDataNodeConnectString(String name){
-		return String.format("jdbc:mysql://localhost/%s?user=test&password=test",name);
+
+	protected String getDataNodeConnectString(String name) {
+		return String.format(
+				"jdbc:mysql://localhost/%s?user=test&password=test", name);
 	}
-	
+
 	protected String getDatabaseName() {
 		return "test";
 	}
-	
+
 	protected void recycleDatabase() {
 		recycleDatabase(getDatabaseName());
 	}
-	
+
 	protected void recycleDatabase(String databaseName) {
-		
-		java.sql.Connection connection = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection( getDatabaseAgnosticConnectString() );
-		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		try {
-			connection.prepareStatement("drop database " + databaseName).execute();
-		}
-		catch (Exception e) {
-			throw new RuntimeException("Unable to drop database " + databaseName,e);
-		}
-		try{
-			connection.prepareStatement("create database " + databaseName).execute();
-			connection.close();
-		}
-		catch (Exception e) {
-			throw new RuntimeException("Unable to drop database " + databaseName,e);
+		if (recycleDatabase) {
+			java.sql.Connection connection = null;
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				connection = DriverManager
+						.getConnection(getDatabaseAgnosticConnectString());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+			try {
+				connection.prepareStatement("drop database " + databaseName)
+						.execute();
+			} catch (Exception e) {
+				throw new RuntimeException("Unable to drop database "
+						+ databaseName, e);
+			}
+			try {
+				connection.prepareStatement("create database " + databaseName)
+						.execute();
+				connection.close();
+			} catch (Exception e) {
+				throw new RuntimeException("Unable to drop database "
+						+ databaseName, e);
+			}
 		}
 	}
-	
+
 	protected String getDatabaseAgnosticConnectString() {
 		return "jdbc:mysql://localhost/?user=test&password=test";
 	}
-	
+
 	protected String getConnectString() {
-		return "jdbc:mysql://localhost/"+getDatabaseName()+"?user=test&password=test";
+		return "jdbc:mysql://localhost/" + getDatabaseName()
+				+ "?user=test&password=test";
 	}
 }
