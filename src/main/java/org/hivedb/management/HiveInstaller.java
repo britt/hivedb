03e53384f340c712a726d38874767110656c601a
Britt Crawford <britt@illtemper.org>
2007-05-04 18:12:21 +0000

    Added argument parsing and enhanced the HiveInstaller
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@302 8f33570f-f526-0410-8023-93ba2dbcbd24

--- src/main/java/org/hivedb/management/HiveInstaller.java
+++ src/main/java/org/hivedb/management/HiveInstaller.java
@@ -2,11 +2,13 @@ package org.hivedb.management;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Map;
 
 import org.hivedb.HiveRuntimeException;
 import org.hivedb.meta.GlobalSchema;
 import org.hivedb.meta.persistence.HiveBasicDataSource;
 import org.hivedb.meta.persistence.HiveSemaphoreDao;
+import org.hivedb.util.GetOpt;
 
 public class HiveInstaller implements Runnable {
 	private String uri;
@@ -25,19 +27,32 @@ public class HiveInstaller implements Runnable {
 	}
 
 	public static void main(String[] argz) {
-		if (argz.length != 1)
+		GetOpt opt = new GetOpt();
+		opt.add("host", true);
+		opt.add("db", true);
+		opt.add("user", true);
+		opt.add("pw", true);
+		
+		Map<String,String> argMap = opt.toMap(argz);
+		if (!opt.validate())
 			throw new IllegalArgumentException(
-					"The hive-installer accepts only one argument, a valid JDBC connection string. "
-							+ argz.length);
+					"Usage: java -jar hivedb-jar-with-dependencies.jar -host <host> -db <database name> -user <username> -pw <password>");
 		else {
 			try{
+				//Tickle driver
 				Class.forName("com.mysql.jdbc.Driver");
 				@SuppressWarnings("unused")
-				Connection conn = DriverManager.getConnection(argz[0].trim());
+				Connection conn = DriverManager.getConnection(getConnectString(argMap));
 			} catch(Exception e) {
 				throw new HiveRuntimeException(e.getMessage());
 			}
-			new HiveInstaller(argz[0].trim()).run();
+			System.out.println(getConnectString(argMap));
+			new HiveInstaller(getConnectString(argMap)).run();
 		}
+	}
+
+	private static String getConnectString(Map<String, String> argMap) {
+		return String.format("jdbc:mysql://%s/%s?user=%s&password=%s", 
+				argMap.get("host"), argMap.get("db"), argMap.get("user"), argMap.get("pw"));
 	}
 }
