8340e18bfacbadfabf2e5f4e317dd711a182f0b4
Britt Crawford <britt@illtemper.org>
2007-05-16 22:58:03 +0000

    Made the installer script a little nicer.  Now you can specify the location of hivedb-jar-with-dependencies.jar by setting the HIVEDB_HOME environment variable.
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@312 8f33570f-f526-0410-8023-93ba2dbcbd24

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-HIVEDB_HOME="$PWD/target"
+if [ -z "$HIVEDB_HOME" ]; then
+	HIVEDB_HOME="$PWD/target"
+fi
 
-java -jar "$HIVEDB_HOME/hivedb-jar-with-dependencies.jar" -host $1 -db $2 -user $3 -pw $4
+if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" ]; then
+	echo "usage: install.sh <host> <database> <user> <password>"
+else
+	java -jar "$HIVEDB_HOME/hivedb-jar-with-dependencies.jar" -host $1 -db $2 -user $3 -pw $4
+fi
