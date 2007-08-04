c345d12c9425251ad908ebd34c348354b07527e9
Britt Crawford <britt@illtemper.org>
2007-08-04 00:46:33 +0000

    Missed during original checkin
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@378 8f33570f-f526-0410-8023-93ba2dbcbd24

---
+++ lib/src/main/java/org/hivedb/meta/ResourceIndex.java
@@ -0,0 +1,17 @@
+package org.hivedb.meta;
+
+import org.hivedb.Hive;
+
+public class ResourceIndex extends SecondaryIndex {
+
+	public ResourceIndex(String name, int type) {
+		this(Hive.NEW_OBJECT_ID,name,type);
+	}
+	
+	public ResourceIndex(int id, String name, int type) {
+		super(id, name, type);
+	}
+
+	@Override
+	public String getTableName() { return getTableName(this.getName(), "id");}
+}
