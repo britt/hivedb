1ec4166b2be7c1c882b2ce69cbbf971673b429cf
Andy Likuski <alikuski@cafepress.com>
2007-12-11 21:05:37 +0000

    removing failed serialization exploration
    git-svn-id: svn+ssh://svn.hivedb.org/svn/hivedb/trunk@508 8f33570f-f526-0410-8023-93ba2dbcbd24

--- src/main/java/org/hivedb/util/serialization/GetFunction.java
+++
@@ -1,5 +0,0 @@
-package org.hivedb.util.serialization;
-
-public interface GetFunction<T> {
-	public abstract T get();
-}
--- src/main/java/org/hivedb/util/serialization/VersionUpgrader.java
+++
@@ -1,9 +0,0 @@
-package org.hivedb.util.serialization;
-
-import org.hivedb.serialization.Modernizer;
-
-public interface VersionUpgrader<T> {
-	boolean isOldVersion(Integer serializedVersion);
-	boolean isImplicitModernization(Integer fromVersion, Integer toVersion);
-	Modernizer getModernizer(Integer fromVersion, Integer toVersion);
-}
