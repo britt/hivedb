f960953c2800036e10ccd2beea66d79a3fd1ca7b
mellwanger <mellwanger@cafepress.com>
2008-05-06 11:56:29 -0700

    Enable DataSource caching

---
+++ src/main/java/org/hivedb/meta/persistence/CachingDataSourceProvider.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.hivedb.meta.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.hivedb.meta.Node;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+public class CachingDataSourceProvider implements DataSourceProvider {
+	
+	private static CachingDataSourceProvider INSTANCE = new CachingDataSourceProvider();
+	
+	private Map<String, LazyConnectionDataSourceProxy> cache = new HashMap<String, LazyConnectionDataSourceProxy>();
+	//private Map<String, DataSource> cache = new HashMap<String, DataSource>();
+	
+	private CachingDataSourceProvider() {
+	}
+	
+	public DataSource getDataSource(Node node) {
+		return getDataSource(node.getUri());
+	}
+
+	public DataSource getDataSource(String uri) {
+		LazyConnectionDataSourceProxy ds = cache.get(uri);
+		//DataSource ds = cache.get(uri);
+		if (ds == null) {
+			ds = new LazyConnectionDataSourceProxy(new HiveBasicDataSource(uri, HiveBasicDataSource.getDefaultPoolSize()));
+			//ds = new HiveBasicDataSource(uri);
+			cache.put(uri, ds);
+		}
+		return ds;
+	}
+	
+	public static CachingDataSourceProvider getInstance() {
+		return INSTANCE;
+	}
+}
