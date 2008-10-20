d003d82705d1ad738e67fcc4f66b12dc89c4dd75
Britt Crawford <britt@illtemper.org>
2008-10-20 11:17:47 -0700

    Began YamlHiveConfigurationLoader...safety commit so I can merge the other branch.

--- src/test/java/org/hivedb/configuration/yaml/YamlHiveConfigurationLoaderTest.java
+++ src/test/java/org/hivedb/configuration/yaml/YamlHiveConfigurationLoaderTest.java
@@ -1,10 +1,13 @@
 package org.hivedb.configuration.yaml;
 
+import org.hivedb.configuration.HiveConfiguration;
 import org.hivedb.util.functional.Maps;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +17,8 @@ import java.util.Map;
 @RunWith(JMock.class)
 public class YamlHiveConfigurationLoaderTest {
   private Mockery mockery;
-  
+  private YamlHiveConfigurationLoader factory;
+
   @Before
   public void setUp() throws Exception {
     mockery = new JUnit4Mockery() {
@@ -22,13 +26,29 @@ public class YamlHiveConfigurationLoaderTest {
         setImposteriser(ClassImposteriser.INSTANCE);
       }
     };
+
+    factory = new YamlHiveConfigurationLoader("/tmp/hive.yml");
   }
 
   @Test
-  public void shouldBuildAHiveConfiguration() throws Exception {}
+  public void shouldBuildAHiveConfiguration() throws Exception {
+    assertNotNull(factory.newInstance());
+  }
 
   @Test
-  public void shouldLoadHiveConfigurationFromAYamlFile() throws Exception {}
+  public void shouldReturnThePathToTheConfigurationFileAsAUri() throws Exception {
+    assertEquals("file:///tmp/hive.yml", factory.getFileUri());
+  }
+
+  private HiveConfiguration fakeHiveConfig() {
+    throw new UnsupportedOperationException("Not yet implemented");
+  }
+
+  @Test
+  public void shouldNotReloadTheConfigurationIfTheFileHasNotBeenModifiedSinceLastLoad() throws Exception {}
+
+  @Test
+  public void shouldThrowIfTheConfigurationFileDoesNotExist() throws Exception {}
 
   private Map<String, Map<String, ?>> fakeYamlConfig() {
     Map<String, Map<String, ?>> yaml = Maps.newHashMap();
